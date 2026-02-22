ddaa03b85f79a7d363bf349054b6fe1c51c26028
themdshakibul <mdshakibul88240@gmail.com>
2026-02-22 12:42:49 +0600

    complte reject issus

--- js/count.js
+++ js/count.js
@@ -1,6 +1,8 @@
 // interview list mt arry
 let interviewList = [];
 let rejectedList = [];
+let currentCount = "all";
+let counts = [];
 
 let total = document.getElementById("total");
 let interview = document.getElementById("interview");
@@ -8,7 +10,7 @@ let rejected = document.getElementById("rejected");
 
 // total jobs
 const totalJobs = document.getElementById("total-job");
-const count = document.getElementById("count");
+const count = document.querySelector(".count");
 
 // all filter btn
 const allFileterBtn = document.getElementById("all-filter-btn");
@@ -30,6 +32,7 @@ function calculateCount() {
   count.innerText = interviewList.length;
 
   rejected.innerText = rejectedList.length;
+  count.innerText = rejectedList.length;
 }
 
 calculateCount();
@@ -47,6 +50,7 @@ function toggle(id) {
   rejectedFilterBtn.classList.remove("bg-blue-500", "text-white");
 
   let selected = document.getElementById(id);
+  currentCount = id;
 
   selected.classList.remove("btn", "text-black");
   selected.classList.add(
@@ -61,6 +65,10 @@ function toggle(id) {
   } else if (id == "all-filter-btn") {
     allcardSections.classList.remove("hidden");
     filterSection.classList.add("hidden");
+  } else if (id == "rejected-filter-btn") {
+    allcardSections.classList.add("hidden");
+    filterSection.classList.remove("hidden");
+    randerRejected();
   }
 }
 
@@ -94,12 +102,53 @@ mainContianers.addEventListener("click", function (evnet) {
     if (!planExistiong) {
       interviewList.push(JobInfo);
     }
+    rejectedList = rejectedList.filter(
+      (item) => item.jobName != JobInfo.jobName,
+    );
     calculateCount();
 
     randerInterview();
+  } else if (evnet.target.classList.contains("rejected-btn")) {
+    const parentNode = evnet.target.parentNode.parentNode;
+    const jobName = parentNode.querySelector(".jobName").innerText;
+    const jobTitle = parentNode.querySelector(".jobTitle").innerText;
+    const remoteJob = parentNode.querySelector(".remoteJob").innerText;
+    const notApplied = parentNode.querySelector(".notApplied").innerText;
+    const peragraph = parentNode.querySelector(".peragraph").innerText;
+
+    parentNode.querySelector(".notApplied").innerText = "REJECTED";
+
+    const JobInfo = {
+      jobName,
+      jobTitle,
+      remoteJob,
+      notApplied: "REJECTED",
+      peragraph,
+    };
+
+    console.log(JobInfo);
+
+    // check list
+    const planExistiong = rejectedList.find(
+      (item) => item.jobName == JobInfo.jobName,
+    );
+
+    if (!planExistiong) {
+      rejectedList.push(JobInfo);
+    }
+    interviewList = interviewList.filter(
+      (item) => item.jobName != JobInfo.jobName,
+    );
+
+    if (currentCount == "interview-filter-btn") {
+      randerInterview();
+    }
+
+    calculateCount();
   }
 });
 
+// randerInterview list
 function randerInterview() {
   filterSection.innerHTML = "";
 
@@ -131,6 +180,65 @@ function randerInterview() {
             </button>
             <p class="peragraph text-xl text-[#323B49]">
               ${interview.peragraph}
+            </p>
+
+            <!-- btn -->
+            <div class="flex gap-5">
+              <button
+                class="interview-btn btn btn-lg border-2 border-green-700 text-xl text-green-500 px-6"
+              >
+                INTERVIEW
+              </button>
+
+              <button
+                class="rejected-btn btn btn-lg border-2 border-red-700 text-xl text-red-500 px-6"
+              >
+                REJECTED
+              </button>
+            </div>
+          </div>
+
+          <!-- part 2 delet btn -->
+          <div class="delet-btn text-2xl">
+            <img src="./assets/delet-btn.png" alt="" class="w-12" />
+          </div>
+    `;
+    filterSection.appendChild(div);
+  }
+}
+
+// randerRejected list
+function randerRejected() {
+  filterSection.innerHTML = "";
+
+  for (let rejected of rejectedList) {
+    console.log(rejected);
+    let div = document.createElement("div");
+    div.className =
+      "cards bg-[#f0f0f0d9] border-2 border-gray-200 p-10 rounded-md mb-10 flex justify-between";
+    div.innerHTML = `
+        <!-- part 1 -->
+          <div class="space-y-5">
+            <div>
+              <h2 class="jobName text-2xl font-bold text-[#002C5C]">
+                ${rejected.jobName}
+              </h2>
+              <p class="jobTitle text-xl text-[#64748B]">
+                ${rejected.jobTitle}
+              </p>
+            </div>
+
+            <p class="remoteJob text-[#64748B] text-xl">
+            ${rejected.remoteJob}
+            </p>
+
+            <button
+              class="notApplied btn btn-soft text-xl btn-2xl text-[#002C5C]"
+            >
+              ${rejected.notApplied}
+            </button>
+            <p class="peragraph text-xl text-[#323B49]">
+              ${rejected.peragraph}
             </p>
 
             <!-- btn -->
