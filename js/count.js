76b67532e7d0cb1ba81ab6f43143646090a29eb1
themdshakibul <mdshakibul88240@gmail.com>
2026-02-22 10:43:48 +0600

    interview section completed

--- js/count.js
+++ js/count.js
@@ -8,7 +8,7 @@ let rejected = document.getElementById("rejected");
 
 // total jobs
 const totalJobs = document.getElementById("total-job");
-console.log(totalJobs);
+const count = document.getElementById("count");
 
 // all filter btn
 const allFileterBtn = document.getElementById("all-filter-btn");
@@ -17,15 +17,17 @@ const rejectedFilterBtn = document.getElementById("rejected-filter-btn");
 
 // all card sections
 const allcardSections = document.getElementById("allCard");
-
 // main container sections
 const mainContianers = document.querySelector("main");
+// filter card sections
+const filterSection = document.getElementById("filterSeciton");
 
 function calculateCount() {
   total.innerText = allcardSections.children.length;
   totalJobs.innerText = allcardSections.children.length;
 
   interview.innerText = interviewList.length;
+  count.innerText = interviewList.length;
 
   rejected.innerText = rejectedList.length;
 }
@@ -53,31 +55,105 @@ function toggle(id) {
     "text-xl",
     "font-semibold",
   );
+  if (id == "interview-filter-btn") {
+    allcardSections.classList.add("hidden");
+    filterSection.classList.remove("hidden");
+  } else if (id == "all-filter-btn") {
+    allcardSections.classList.remove("hidden");
+    filterSection.classList.add("hidden");
+  }
 }
 
 //main container sections
 mainContianers.addEventListener("click", function (evnet) {
-  const parentNode = evnet.target.parentNode.parentNode;
-  const jobName = parentNode.querySelector(".jobName").innerText;
-  const jobTitle = parentNode.querySelector(".jobTitle").innerText;
-  const remoteJob = parentNode.querySelector(".remoteJob").innerText;
-  const notApplied = parentNode.querySelector(".notApplied").innerText;
-  const peragraph = parentNode.querySelector(".peragraph").innerText;
+  if (evnet.target.classList.contains("interview-btn")) {
+    const parentNode = evnet.target.parentNode.parentNode;
+    const jobName = parentNode.querySelector(".jobName").innerText;
+    const jobTitle = parentNode.querySelector(".jobTitle").innerText;
+    const remoteJob = parentNode.querySelector(".remoteJob").innerText;
+    const notApplied = parentNode.querySelector(".notApplied").innerText;
+    const peragraph = parentNode.querySelector(".peragraph").innerText;
 
-  const JobInfo = {
-    jobName,
-    jobTitle,
-    remoteJob,
-    notApplied,
-    peragraph,
-  };
+    parentNode.querySelector(".notApplied").innerText = "INTERVIEW";
 
-  console.log(JobInfo);
+    const JobInfo = {
+      jobName,
+      jobTitle,
+      remoteJob,
+      notApplied: "INTERVIEW",
+      peragraph,
+    };
 
-  const planExistiong = interviewList.find(
-    (item) => item.jobName == JobInfo.jobName,
-  );
-  if (!planExistiong) {
-    interviewList.push(JobInfo);
+    console.log(JobInfo);
+
+    // check list
+    const planExistiong = interviewList.find(
+      (item) => item.jobName == JobInfo.jobName,
+    );
+
+    if (!planExistiong) {
+      interviewList.push(JobInfo);
+    }
+    calculateCount();
+
+    randerInterview();
   }
 });
+
+function randerInterview() {
+  filterSection.innerHTML = "";
+
+  for (let interview of interviewList) {
+    console.log(interview);
+    let div = document.createElement("div");
+    div.className =
+      "cards bg-[#f0f0f0d9] border-2 border-gray-200 p-10 rounded-md mb-10 flex justify-between";
+    div.innerHTML = `
+        <!-- part 1 -->
+          <div class="space-y-5">
+            <div>
+              <h2 class="jobName text-2xl font-bold text-[#002C5C]">
+                ${interview.jobName}
+              </h2>
+              <p class="jobTitle text-xl text-[#64748B]">
+                ${interview.jobTitle}
+              </p>
+            </div>
+
+            <p class="remoteJob text-[#64748B] text-xl">
+            ${interview.remoteJob}
+            </p>
+
+            <button
+              class="notApplied btn btn-soft text-xl btn-2xl text-[#002C5C]"
+            >
+              ${interview.notApplied}
+            </button>
+            <p class="peragraph text-xl text-[#323B49]">
+              ${interview.peragraph}
+            </p>
+
+            <!-- btn -->
+            <div class="flex gap-5">
+              <button
+                class="interview-btn btn btn-lg border-2 border-green-700 text-xl text-green-500 px-6"
+              >
+                INTERVIEW
+              </button>
+
+              <button
+                class="rejected-btn btn btn-lg border-2 border-red-700 text-xl text-red-500 px-6"
+              >
+                REJECTED
+              </button>
+            </div>
+          </div>
+
+          <!-- part 2 delet btn -->
+          <div class="delet-btn text-2xl">
+            <img src="./assets/delet-btn.png" alt="" class="w-12" />
+          </div>
+    `;
+    filterSection.appendChild(div);
+  }
+}
